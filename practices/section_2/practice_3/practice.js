9e7b4cd99ea06cf7152b9e58e22fc590ea9f5236
RedXiang <814893024@qq.com>
2018-04-25 20:29:10 +0800

    4-25

--- practices/section_2/practice_3/practice.js
+++ practices/section_2/practice_3/practice.js
@@ -1,31 +1,55 @@
 function count_same_elements(collection) {
   //在这里写入代码
   var result = [{ name: collection[0], summary: 1 }];
-  var collection_noRepeat = [collection[0]];
   var k = 0;
+  var collection_noRepeat = [collection[0]];
+  var object = [];
+  var pattern1 = new RegExp(/([a-z])\[([0-9]+)]/);
+  var pattern2 = new RegExp("([a-z])-([0-9]+)");
+  var pattern3 = new RegExp("([a-z]):([0-9]+)");
   for (var i = 1; i < collection.length; i++) {
-    var repeat = false;
-    if (collection[i].length > 1) {
-      collection[i] = collection[i].split("-");
-      for (var l = 1; l < collection[i][1]; l++) {
-        collection.push(collection[i][0]);
-      }
-      collection[i] = collection[i][0];
+    if (pattern1.test(collection[i])) {
+      match = pattern1.exec(collection[i]);
+      object.push({
+        name: match[1],
+        summary: match[2]
+      });
     }
-    for (var j = 0; j < collection_noRepeat.length; j++) {
-      if (collection[i] == collection_noRepeat[j]) {
-        result[k].summary+= 1;
-        repeat = true;
-        break;
-      }
+    else if (pattern2.test(collection[i])) {
+      match = pattern2.exec(collection[i]);
+      object.push({
+        name: match[1],
+        summary: match[2]
+      });
     }
-    if (!repeat) {
-      collection_noRepeat.push(collection[i]);
-      result.push({ name: collection[i],summary: 1 });
-      k++;
+    else if (pattern3.test(collection[i])) {
+      match = pattern3.exec(collection[i]);
+      object.push({
+        name: match[1],
+        summary: match[2]
+      });
+    }
+    else {
+      var repeat = false;
+      for (var j = 0; j < collection_noRepeat.length; j++) {
+        if (collection[i] == collection_noRepeat[j]) {
+          repeat = true;
+          result[k].summary += 1;
+        }
+      }
+      if (!repeat) {
+        collection_noRepeat.push(collection[i]);
+        result.push({ name: collection[i], summary: 1 });
+        k++;
+      }
+
     }
   }
-  return result;
+  console.log(object);
+
+
 }
+
+
 
 module.exports = count_same_elements;
