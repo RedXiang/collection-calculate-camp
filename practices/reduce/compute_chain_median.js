4eb7bed3a10001b4bb6988f1db8c31e8ac69d618
RedXiang <814893024@qq.com>
2018-04-30 14:59:12 +0800

    4-30

--- practices/reduce/compute_chain_median.js
+++ practices/reduce/compute_chain_median.js
@@ -2,18 +2,28 @@
 
 function compute_chain_median(collection) {
   //在这里写入代码
-  var collection_a = [];
+  var collection_new = collection.split("->");
   var result;
-  for (var i = 0; i < collection.length; i + 3) {
-    collection_a.push(collection[i]);
+  for (var j = 0; j < collection_new.length; j++) {
+    collection_new[j] = parseInt(collection_new[j]);
   }
-  if (collection_a.length % 2 != 0) {
-    result = collection_a[(collection_a.length + 1) / 2];
+  for (var k = 0; k < collection_new.length - 1; k++) {
+    for (var l = k + 1; l < collection_new.length; l++) {
+      if (collection_new[k] > collection_new[l]) {
+        var temp = collection_new[k];
+        collection_new[k] = collection_new[l];
+        collection_new[l] = temp;
+      }
+    }
+  }
+  var i = collection_new.length;
+  if (i % 2 == 0) {
+    result = (collection_new[i/2] + collection_new[i/2-1]) / 2;
   }
   else {
-    var sum = collection_a[collection_a.length / 2] + collection_a[collection_a.length / 2 + 1];
-    result = sum / 2;
+    result = collection_new[(i - 1) / 2];
   }
+  return result;
 }
 
 module.exports = compute_chain_median;
--- practices/section_2/practice_3/practice.js
+++ practices/section_2/practice_3/practice.js
@@ -1,52 +1,47 @@
 function count_same_elements(collection) {
   //在这里写入代码
   var result = [{ name: collection[0], summary: 1 }];
-  var k = 0;
-  var collection_noRepeat = [collection[0]];
-  var object = [];
   var pattern1 = new RegExp(/([a-z])\[([0-9]+)]/);
   var pattern2 = new RegExp("([a-z])-([0-9]+)");
   var pattern3 = new RegExp("([a-z]):([0-9]+)");
-  for (var i = 1; i < collection.length; i++) {
+  for (var i = 0; i < collection.length; i++) {
     if (pattern1.test(collection[i])) {
       match = pattern1.exec(collection[i]);
-      object.push({
-        name: match[1],
-        summary: match[2]
-      });
+      collection[i] = match[1];
+      for (var l = 1; l < parseInt(match[2]); l++) {
+        collection.push(match[1]);
+      }
     }
     else if (pattern2.test(collection[i])) {
       match = pattern2.exec(collection[i]);
-      object.push({
-        name: match[1],
-        summary: match[2]
-      });
+      collection[i] = match[1];
+      for (var m = 1; m < parseInt(match[2]); m++) {
+        collection.push(match[1]);
+      }
     }
     else if (pattern3.test(collection[i])) {
       match = pattern3.exec(collection[i]);
-      object.push({
-        name: match[1],
-        summary: match[2]
-      });
-    }
-    else {
-      var repeat = false;
-      for (var j = 0; j < collection_noRepeat.length; j++) {
-        if (collection[i] == collection_noRepeat[j]) {
-          repeat = true;
-          result[k].summary += 1;
-        }
+      collection[i] = match[1];
+      for (var n = 1; n < parseInt(match[2]); n++) {
+        collection.push(match[1]);
       }
-      if (!repeat) {
-        collection_noRepeat.push(collection[i]);
-        result.push({ name: collection[i], summary: 1 });
-        k++;
-      }
-
     }
   }
-  console.log(object);
 
+  for (var o = 1; o < collection.length; o++) {
+    var repeat = false;
+    for (var j = 0; j < result.length; j++) {
+      if (collection[o] == result[j].name) {
+        repeat = true;
+        result[j].summary += 1;
+      }
+    }
+    if (!repeat) {
+      result.push({ name: collection[o], summary: 1 });
+    }
+  }
+
+  return result;
 
 }
 
